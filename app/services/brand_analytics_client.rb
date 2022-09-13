0c22f184af180cfb4b1ff64e8b66a6b7944a37d0
sleepingming <daniltop1@bk.ru>
2022-09-13 16:52:41 +0300

    fixed api method

--- app/services/brand_analytics_client.rb
+++ app/services/brand_analytics_client.rb
@@ -6,7 +6,8 @@ class BrandAnalyticsClient
   #URL = 'https://bans-api.br-analytics.ru/ba_api/feed.messages?themeId=12489954&filter[ft][1]&filter[ftone][1]=on&timeFrom=1659981754&timeTo=1662660154&offset=0'
   #URL = 'https://bans-api.br-analytics.ru/ba_api/statistic.topStatRegionTone?themeId=12489954&df=2022-08-08 00:00&dt=2022-09-08 23:59&filter[ft][1]=on'
   #URL = 'https://bans-api.br-analytics.ru/ba_api/statistic.topStatTone?themeId=12489954&df=2022-08-08 00:00&dt=2022-09-08 23:59&filter[ft][1]=on'
-  URL = 'https://bans-api.br-analytics.ru/ba_api/statistic.dateline?themeId=12489954&df=2022-08-08 00:00&dt=2022-09-08 23:59&filter[ft][1]'
+  #URL = 'https://bans-api.br-analytics.ru/ba_api/statistic.dateline?themeId=12489954&df=2022-08-08 00:00&dt=2022-09-08 23:59&filter[ft][1]'
+  URL = 'https://bans-api.br-analytics.ru/ba_api/statistic.dateLineTone?themeId=12489954&df=2022-08-08 00:00&dt=2022-09-08 23:59&filter[ft][1]=on'
 
   def initialize(token)
     @token = token
--- app/services/get_analytics_service.rb
+++ app/services/get_analytics_service.rb
@@ -1,7 +1,7 @@
 class GetAnalyticsService
   def call
     client = BrandAnalyticsClient.new(Rails.application.credentials.access_token)
-    client.data_get["data"].each do |key, value|
+    client.data_get.dig("data", "1").each do |key, value|
       HeinzDatum.where(date: key).first_or_create(count: value)
     end
   end
